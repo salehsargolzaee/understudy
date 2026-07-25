f8ecf04a7e603d11bc75a2b07bee5f618d8cbc09
Saleh Sargolzaee <salehsargolzaee@gmail.com>
2026-07-24 23:07:01 -0700

    Add page analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "katex/dist/katex.min.css";
 import "./index.css";
 import App from "./components/App";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
